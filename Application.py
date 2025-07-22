3999ce6d9d27db1f7bcff8a5624654fea6c6eec0
Shachi Shukla <shachi.03shukla@gmail.com>
2025-07-22 14:20:34 +0530

    application modified

--- Application.py
+++ Application.py
@@ -14,21 +14,21 @@ with open('ridge.pkl', 'rb') as f:
     ridge = pickle.load(f)
 
 ## Take Inputs
-temp = st.number_input("temperature ", step = 1, min_value= 22 , max_value= 42)
+temp = st.number_input("Enter temperature [in range 22-42] (degree Celsius) ", step = 1, min_value= 22 , max_value= 42)
 
-RH =  st.number_input("Relative Humditiy ", step = 1 , min_value= 21 , max_value = 90)
+RH =  st.number_input("Enter Relative Humditiy [in range 21-90] (%)", step = 1 , min_value= 21 , max_value = 90)
 
-ws = st.number_input("Wind speed in km/h ", step = 1 , min_value=6 , max_value=29)
+ws = st.number_input("Enter Wind speed [in range 6-29] (km/h) ", step = 1 , min_value=6 , max_value=29)
 
-rain = st.number_input('Rain: total day in mm' , step = 0.1 , min_value=0.0, max_value=16.8)
+rain = st.number_input('Rain: total day [in range 0.0-16.8] (mm)' , step = 0.1 , min_value=0.0, max_value=16.8)
 st.write(' Fire Weather (FWI) Components')
-FFMC = st.number_input('Fine Fuel Moisture Code (FFMC) index from the FWI system', step = 0.1, min_value=28.6 , max_value=92.5)
+FFMC = st.number_input('Fine Fuel Moisture Code (FFMC) index from the FWI system [in range 28.6-92.5] ', step = 0.1, min_value=28.6 , max_value=92.5)
 
-DMC = st.number_input('Duff Moisture Code (DMC) index from the FWI system ', step = 0.1 , min_value=1.1 , max_value=65.9)
+DMC = st.number_input('Duff Moisture Code (DMC) index from the FWI system [in range 1.1-65.9]', step = 0.1 , min_value=1.1 , max_value=65.9)
 
-ISI = st.number_input('Initial Spread Index (ISI) index from the FWI system ' , step = 0.1 , min_value=0.0 , max_value=18.5)
+ISI = st.number_input('Initial Spread Index (ISI) index from the FWI system [in range 0.0-18.5]' , step = 0.1 , min_value=0.0 , max_value=18.5)
 
-classes = st.selectbox('Class , fire (1) or not fire (0)', [1 , 0] )
+classes = st.selectbox('Class , 1 for Fire or 0 for Not Fire', [1 , 0] )
 region = st.selectbox('Region' , ['Bejaia Region' , 'Sidi-Bel Abbes Region'])
 
 region_encoded = 1 if region == 'Bejaia Region' else 0
